0c50022b8f8c32d966778dde719f916df005d3f9
夜夜 <vcde0000@gmail.com>
2026-02-08 09:27:17 +0800

    chore: change winston to pino

--- index.js
+++ index.js
@@ -22,34 +22,65 @@ class Plugin {
 
   onLoad() {
     const { TREM, Logger, logger } = this.#ctx;
+    // 假設這裡的 CustomLogger 已經適配或是通用的，若它是純 Winston 依賴可能也需要調整
     const { CustomLogger } = require("../logger/logger").createCustomLogger(Logger);
     this.logger = new CustomLogger("disable_tts");
 
     this.logger.info("Initialization completed!");
 
+    // 初始 Mock
     TREM.variable.speech = {
-      speak    : () => void 0,
-      speaking : () => void 0,
-      cancel   : () => void 0,
+      speak: () => void 0,
+      speaking: () => void 0,
+      cancel: () => void 0,
     };
 
-    const winstonLogger = logger._getLogger();
+    // 獲取 Pino 實例
+    const pinoLogger = logger._getLogger();
 
-    winstonLogger.transports.forEach(transport => {
-      const originalLog = transport.log.bind(transport);
-      transport.log = (info, callback) => {
-        if (!this.disable && info.message == "Speech ready!") {
-          this.disable = true;
-          this.logger.info("Disabling TTS successfully!");
-          TREM.variable.speech = {
-            speak    : () => void 0,
-            speaking : () => void 0,
-            cancel   : () => void 0,
-          };
-        }
-        return originalLog(info, callback);
-      };
-    });
+    /**
+     * PINO 修改部分：
+     * Pino 不像 Winston 有 transports 陣列可以遍歷。
+     * 我們需要攔截觸發 "Speech ready!" 的日誌方法（通常是 info）。
+     */
+
+    // 1. 綁定原始的 info 方法以保留上下文
+    const originalInfo = pinoLogger.info.bind(pinoLogger);
+
+    // 2. 覆寫 info 方法
+    pinoLogger.info = (...args) => {
+      // Pino 的參數形式通常是: (msg), (obj, msg), 或 (obj)
+      // 我們需要檢查參數中是否包含目標訊息
+      let isTargetMessage = false;
+
+      // 檢查第一個參數是字串的情況: logger.info("Speech ready!")
+      if (typeof args[0] === 'string' && args[0] === "Speech ready!") {
+        isTargetMessage = true;
+      }
+      // 檢查第二個參數是字串的情況: logger.info({context}, "Speech ready!")
+      else if (typeof args[1] === 'string' && args[1] === "Speech ready!") {
+        isTargetMessage = true;
+      }
+      // 檢查物件中包含 msg 屬性的情況: logger.info({ msg: "Speech ready!" })
+      else if (args[0] && typeof args[0] === 'object' && args[0].msg === "Speech ready!") {
+        isTargetMessage = true;
+      }
+
+      // 如果檢測到目標訊息且尚未禁用
+      if (!this.disable && isTargetMessage) {
+        this.disable = true;
+        this.logger.info("Disabling TTS successfully!");
+
+        TREM.variable.speech = {
+          speak: () => void 0,
+          speaking: () => void 0,
+          cancel: () => void 0,
+        };
+      }
+
+      // 3. 執行原始的日誌記錄，確保日誌仍被輸出
+      return originalInfo(...args);
+    };
   }
 }
 
